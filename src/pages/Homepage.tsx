d9e4e5d363e22f748702dd037ec96e8e07f79e8b
Ryan Kusuma <ryankusuma147@gmail.com>
2024-11-20 11:32:43 +0700

    [UPDATE] Change Homapage UI

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { ArrowRightIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
